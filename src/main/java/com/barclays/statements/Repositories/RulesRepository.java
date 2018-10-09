303edd0983fd1a4275d80dbe2e8c997f5f78c215
imfrank2 <sitrea0@gmail.com>
2018-10-09 15:44:10 +0530

    Added deletion and search api

--- src/main/java/com/barclays/statements/Repositories/RulesRepository.java
+++ src/main/java/com/barclays/statements/Repositories/RulesRepository.java
@@ -2,7 +2,8 @@ package com.barclays.statements.Repositories;
 
 import com.barclays.statements.Models.Rules;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface RulesRepository extends MongoRepository<Rules, String> {
 }
--- src/main/java/com/barclays/statements/RulesController.java
+++ src/main/java/com/barclays/statements/RulesController.java
@@ -3,15 +3,12 @@ package com.barclays.statements;
 import com.barclays.statements.Models.Rules;
 import com.barclays.statements.Repositories.RulesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import org.bson.types.ObjectId;
-import org.springframework.data.annotation.Id;
 
-import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin
@@ -35,6 +32,16 @@ public class RulesController {
     @RequestMapping(value ="/addRule", method = RequestMethod.POST)
     public void addRule(@RequestParam(name="rule") String rule){
         this.repository.insert(new Rules(new ObjectId(), rule));
+    }
+
+    @RequestMapping(value = "/getRule/{id}", method = RequestMethod.GET)
+    public Optional<Rules> getRule(@PathVariable String id){
+        return this.repository.findById(id);
+    }
+
+    @RequestMapping(value = "/deleteRule/{id}", method = RequestMethod.DELETE)
+    public void deleteRule(@PathVariable String id){
+        this.repository.deleteById(id);
     }
 
 }
